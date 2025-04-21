7c7048a467e044e8488ad6c1e57e8329b5da6f72
vic <victorstancioiu@gmail.com>
2025-04-21 21:39:25 +0200

    remove temp project

--- TesterApp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReplayViewer;
-using ReplayViewer.ReplayDrawers;
-
-const string replayPath = @"C:\Users\Victor\Desktop\mvlo stuff\Replay-1744507719.mvlreplay";
-var replay = new BinaryReplayMatch(File.OpenRead(replayPath));
-if (replay.Valid) replay.Start(new VideoReplayDrawer(true));
-else Console.WriteLine("Invalid replay file.");
